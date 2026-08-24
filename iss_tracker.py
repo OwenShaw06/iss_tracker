f1e95f1e1cda6a792aa0c6244bc836ef174940d1
Owen Shaw <owen.w.shaw@gmail.com>
2026-08-24 15:09:08 -0600

    Looped it every 5 seconds

--- iss_tracker.py
+++ iss_tracker.py
@@ -1,34 +1,35 @@
 import requests
 from math import radians, sin, cos, sqrt, atan2
-
-response = requests.get("http://api.open-notify.org/iss-now.json")
-
-data = response.json()
-
-print(type(data))
-lat = data['iss_position']['latitude']
-lon = data['iss_position']['longitude']
-
+import time
 
 my_lat = 40.3641
 my_lon = -111.7385
 
-pin = requests.get(f"https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={lat}&longitude={lon}&localityLanguage=en")
 
-location = pin.json()
-country = location.get('countryName', 'Unknown')
 
-def calculate_distance(lat1, lon1, lat2, lon2):
-    R = 6371  # Earth's radius in kilometers
-    lat1, lon1, lat2, lon2 = radians(lat1), radians(lon1), radians(lat2), radians(lon2)
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return R * c
+while True: 
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    data = response.json()
+    lat = data['iss_position']['latitude']
+    lon = data['iss_position']['longitude']
+    print(data['iss_position'])
+    pin = requests.get(f"https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={lat}&longitude={lon}&localityLanguage=en")
 
-distance = calculate_distance(my_lat, my_lon, float(lat), float(lon))
+    location = pin.json()
+    country = location.get('countryName') or 'an ocean'
 
-print(f"The ISS is currently at latitude {lat} and longitude {lon}.")
-print(f"Rougly {distance:.0f} km away from you.")
-print(f"Sitting over {country} right now.")
+    def calculate_distance(lat1, lon1, lat2, lon2):
+        R = 6371  # Earth's radius in kilometers
+        lat1, lon1, lat2, lon2 = radians(lat1), radians(lon1), radians(lat2), radians(lon2)
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
+        a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return R * c
+
+    distance = calculate_distance(my_lat, my_lon, float(lat), float(lon))
+
+    print(f"The ISS is currently at latitude {lat} and longitude {lon}.")
+    print(f"Rougly {distance:.0f} km away from you.")
+    print(f"Sitting over {country} right now.")
+    time.sleep(5)
